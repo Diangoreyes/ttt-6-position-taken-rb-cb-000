a8b4b7c4ff76a455c0533fb221c9ca6ef757809d
Diango Reyes <ditzun@gmail.com>
2017-09-20 17:30:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
-  elsif bboard[index] == nil
+  elsif board[index] == nil
     retunr false
   else board[index] == "X" && board[index] == "0"
     return true
